a661e3acd29db35fcf88c54a357ac6f972f6688b
Cooper Orio <cooper.orio@columbia.edu>
2025-05-31 22:15:14 -0400

    Cleaned the last commit

--- data_collection/scraper.py
+++ data_collection/scraper.py
@@ -4,9 +4,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from datetime import date, datetime, timedelta
 import time
